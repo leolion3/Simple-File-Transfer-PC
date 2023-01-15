aee23d2c81755c728124ad22afde9c16ea866981
Leonard <leonard.haddad@adesso.de>
2023-01-15 17:09:02 +0200

    feat: add share plugin for fat jar

---
+++ src/main/java/software/isratech/easy_file_transferer/GUIStarter.java
@@ -0,0 +1,9 @@
+package software.isratech.easy_file_transferer;
+
+/** Extra main class for bundles jarfile */
+public class GUIStarter {
+
+    public static void main(final String[] args) {
+        EasyFileTransferApplication.main(args);
+    }
+}
